9e7f97e1fd07ef7d8224451fa9b80a1c210277df
mamaevann <mamaeva.naidag@mail.ru>
2022-02-25 22:05:21 +0300

    CustomTableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Наида Мамаева on 25.02.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+ 
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
